714b744d77a43f0c09fb2a463a534bcdba817a93
Valentin Minkov <minkowalentin@proton.me>
2023-03-13 23:49:48 +0100

    update content

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,10 +7,64 @@ export default function About() {
     <div>
       <div className={styles.introductionContainer}>
         <Image src={"/images/me.jpeg"} alt="me" className={styles.portrait} />
-        <span>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum dicta
-          nisi quos eum deleniti suscipit.
-        </span>
+        <h1>Skills and Expertise:</h1>
+        <p>
+          As a web developer, I specialize in several areas of expertise and
+          have gained proficiency in a range of technologies. My main skills, in
+          decreasing order of expertise level, include:
+        </p>
+        <ul>
+          <li>
+            <strong>React:</strong> I have extensive experience building modern,
+            dynamic user interfaces using the React library. From simple
+            components to complex web applications, I'm comfortable working with
+            React to create custom solutions that meet clients' needs.
+          </li>
+          <li>
+            <strong>JavaScript:</strong> I'm proficient in using JavaScript to
+            create interactive and responsive web pages. I'm also skilled in
+            working with popular frameworks and libraries, such as jQuery and
+            Vue.js.
+          </li>
+          <li>
+            <strong>TypeScript:</strong> I have experience using TypeScript to
+            build scalable, maintainable web applications with improved type
+            safety.
+          </li>
+          <li>
+            <strong>Angular:</strong> I have worked on several projects using
+            Angular, a popular framework for building dynamic single-page
+            applications.
+          </li>
+          <li>
+            <strong>Node.js:</strong> I have experience building server-side
+            applications using Node.js, and I'm comfortable working with popular
+            frameworks and libraries, such as Express and Socket.io.
+          </li>
+          <li>
+            <strong>HTML:</strong> I have extensive knowledge of HTML and its
+            latest specifications, including HTML5.
+          </li>
+          <li>
+            <strong>SCSS:</strong> I'm skilled in using SCSS to create flexible
+            and reusable styles for web pages and applications.
+          </li>
+          <li>
+            <strong>SQL:</strong> I have experience working with relational
+            databases and writing complex SQL queries to extract, transform, and
+            analyze data.
+          </li>
+          <li>
+            <strong>Golang:</strong> I'm comfortable working with Go, a powerful
+            and efficient programming language commonly used for building web
+            applications.
+          </li>
+        </ul>
+        <p>
+          In addition, I'm also comfortable with native development and enjoy
+          picking up new languages and frameworks as needed to meet the needs of
+          each project.
+        </p>
       </div>
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,9 +4,40 @@ import styles from "./Home.module.scss";
 export default function Home() {
   return (
     <div className={styles.container}>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia iure minus
-      suscipit atque dignissimos exercitationem quibusdam obcaecati hic ex odio
-      quas, repellendus molestias error quaerat sint repellat ea ad natus.
+      <h1> Building Better Online Experiences </h1>
+      <div>
+        <h1>Welcome to My Portfolio</h1>
+        <p>
+          Hi, I'm Valentin Minkov, a passionate web developer with a focus on
+          crafting engaging and responsive websites.
+        </p>
+
+        <p>
+          My expertise in web development spans both front-end and full-stack
+          solutions, and I'm committed to building custom web solutions that
+          align with clients' needs and goals. From concept to creation, I'm
+          dedicated to creating better online experiences that resonate with
+          audiences.
+        </p>
+
+        <p>
+          If you want to learn more about me and my work, be sure to check out
+          my "About" section. There, you'll find more information about my
+          background, experience, and approach to web development.
+        </p>
+
+        <p>
+          And if you're interested in seeing some of my recent projects and the
+          kind of work I can do, head over to my "Projects" section. You'll find
+          a selection of projects that showcase my skills and expertise in
+          creating intuitive, user-friendly websites.
+        </p>
+
+        <p>
+          Thanks for stopping by, and I look forward to connecting with you
+          soon!
+        </p>
+      </div>
     </div>
   );
 }
